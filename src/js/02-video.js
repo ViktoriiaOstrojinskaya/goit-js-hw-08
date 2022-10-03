c996a71db048a252991a1b9477078ced68ea52b8
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-02 21:17:50 -0400

    task #2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,43 +1,15 @@
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-const onPlay = function (data) {
-  player
-    .getCurrentTime()
-    .then(function (seconds) {
-      console.log(seconds);
-      // seconds = the current playback position
-    })
-    .catch(function (error) {
-      // an error occurred
-      console.log(error);
-    });
-  // data is an object containing properties specific to that event
+const options = {
+  id: 'vimeo-player',
+  width: 'width',
 };
 
-player.on('play', onPlay);
+const player = new Vimeo.Player('vimeo-player', options);
 
-player
-  .setCurrentTime()
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+player.setVolume(0);
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.on('play', function () {
+  console.log('Played the first video');
+  setCurrentTime(seconds);
+});
