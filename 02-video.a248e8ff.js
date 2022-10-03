909652ce75a81481eb01bfec6bea73f054ae3927
ViktoriiaOstrojinskaya <99836486+ViktoriiaOstrojinskaya@users.noreply.github.com>
2022-10-03 01:18:16 +0000

    Deploying to gh-pages from @ ViktoriiaOstrojinskaya/goit-js-hw-08@c996a71db048a252991a1b9477078ced68ea52b8 🚀

---
+++ 02-video.a248e8ff.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("iframe");var e=new Vimeo.Player("vimeo-player",{id:"vimeo-player",width:"width"});e.setVolume(0),e.on("play",(function(){console.log("Played the first video"),setCurrentTime(seconds)}))}();
+//# sourceMappingURL=02-video.a248e8ff.js.map
