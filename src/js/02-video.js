3a8c941078fa6e823e61bab76e2dcd5e27f20618
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-04 09:08:43 -0400

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import local from './local-storage';
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-const iframe = document.querySelector('#vimeo-player');
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 
@@ -12,4 +12,4 @@ const onPlay = function (data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
